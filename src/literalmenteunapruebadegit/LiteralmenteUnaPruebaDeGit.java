2c3868645194f3551f8e2b5ad1b98bad3e4f3ac9
FrancoRV <francoagustin433@gmail.com>
2019-07-30 10:24:01 -0300

    first commit

---
+++ src/literalmenteunapruebadegit/LiteralmenteUnaPruebaDeGit.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package literalmenteunapruebadegit;
+
+/**
+ *
+ * @author DELL
+ */
+public class LiteralmenteUnaPruebaDeGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+        System.out.println("Hola, aca no va a haber nada, solo es una prueba para git.");
+        
+    }
+    
+}
